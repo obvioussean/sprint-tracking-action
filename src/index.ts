8ebb8646cfaa958fb9fd5a6a9717c5c3369d1030
Sean Ferguson <obvioussean@github.com>
2023-12-18 10:25:46 -0800

    Add retry on rate limiting

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,39 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 import { Project, Roadmap } from './roadmap';
+import { throttling } from '@octokit/plugin-throttling';
+import { Octokit } from '@octokit/rest';
 
+const ThrottledOctokit = Octokit.plugin(throttling);
 
 async function run(): Promise<void> {
     const token = `${process.env.PAT_TOKEN}`;
-    const graphql = github.getOctokit(token).graphql;
+    const octoKit = new ThrottledOctokit({
+        auth: token,
+        previews: ["cloak"],
+        throttle: {
+            onRateLimit: (retryAfter: number, options: any, octokit: any) => {
+                octokit.log.warn(
+                    `Request quota exhausted for request ${options.method} ${options.url}`
+                );
+
+                octokit.log.info(`Retrying after ${retryAfter} seconds for the ${options.request.retryCount} time!`);
+
+                return true;
+            },
+            onSecondaryRateLimit: (retryAfter: number, options: any, octokit: any) => {
+                // does not retry, only logs a warning
+                octokit.log.warn(
+                    `Abuse detected for request ${options.method} ${options.url}`
+                );
+
+                octokit.log.info(`Retrying after ${retryAfter} seconds for the ${options.request.retryCount} time!`);
+
+                return true;
+            },
+        },
+    });
+    const graphql = octoKit.graphql;
 
     const owner = github.context.repo.owner;
     const repo = github.context.repo.repo;
